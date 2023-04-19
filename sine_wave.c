779a161e2c76d4c01a408468b18154d6a12efe55
Jay Wiggins <5948+funkfinger@users.noreply.github.com>
2023-04-19 15:47:16 -0700

    sine wave example from https://github.com/elehobica/pico_sine_wave_i2s_32b

---
+++ sine_wave.c
@@ -0,0 +1,214 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#define PICO_AUDIO_I2S_DATA_PIN 9
+#define PICO_AUDIO_I2S_CLOCK_PIN_BASE 10
+
+
+#include <stdio.h>
+#include <math.h>
+
+#if PICO_ON_DEVICE
+
+#include "hardware/pll.h"
+#include "hardware/clocks.h"
+#include "hardware/structs/clocks.h"
+
+#endif
+
+#include "pico/stdlib.h"
+
+#if USE_AUDIO_I2S
+
+#include "pico/audio_i2s.h"
+
+#elif USE_AUDIO_PWM
+#include "pico/audio_pwm.h"
+#elif USE_AUDIO_SPDIF
+#include "pico/audio_spdif.h"
+#endif
+
+#define SINE_WAVE_TABLE_LEN 2048
+#define SAMPLES_PER_BUFFER 1156 // Samples / channel
+
+static const uint32_t PIN_DCDC_PSM_CTRL = 23;
+
+static int16_t sine_wave_table[SINE_WAVE_TABLE_LEN];
+audio_buffer_pool_t *ap;
+uint32_t step0 = 0x200000;
+uint32_t step1 = 0x200000;
+uint32_t pos0 = 0;
+uint32_t pos1 = 0;
+const uint32_t pos_max = 0x10000 * SINE_WAVE_TABLE_LEN;
+uint vol = 20;
+
+audio_buffer_pool_t *init_audio() {
+
+    static audio_format_t audio_format = {
+            .pcm_format = AUDIO_PCM_FORMAT_S32,
+            .sample_freq = 44100,
+            .channel_count = 2
+    };
+
+    static audio_buffer_format_t producer_format = {
+            .format = &audio_format,
+            .sample_stride = 8
+    };
+
+    audio_buffer_pool_t *producer_pool = audio_new_producer_pool(&producer_format, 3,
+                                                                      SAMPLES_PER_BUFFER); // todo correct size
+    bool __unused ok;
+    const audio_format_t *output_format;
+#if USE_AUDIO_I2S
+    audio_i2s_config_t config = {
+            .data_pin = PICO_AUDIO_I2S_DATA_PIN,
+            .clock_pin_base = PICO_AUDIO_I2S_CLOCK_PIN_BASE,
+            .dma_channel = 0,
+            .pio_sm = 0
+    };
+
+    output_format = audio_i2s_setup(&audio_format, &audio_format, &config);
+    if (!output_format) {
+        panic("PicoAudio: Unable to open audio device.\n");
+    }
+
+    ok = audio_i2s_connect(producer_pool);
+    assert(ok);
+    { // initial buffer data
+        audio_buffer_t *buffer = take_audio_buffer(producer_pool, true);
+        int32_t *samples = (int32_t *) buffer->buffer->bytes;
+        for (uint i = 0; i < buffer->max_sample_count; i++) {
+            samples[i*2+0] = 0;
+            samples[i*2+1] = 0;
+        }
+        buffer->sample_count = buffer->max_sample_count;
+        give_audio_buffer(producer_pool, buffer);
+    }
+    audio_i2s_set_enabled(true);
+#elif USE_AUDIO_PWM
+    output_format = audio_pwm_setup(&audio_format, -1, &default_mono_channel_config);
+    if (!output_format) {
+        panic("PicoAudio: Unable to open audio device.\n");
+    }
+    ok = audio_pwm_default_connect(producer_pool, false);
+    assert(ok);
+    audio_pwm_set_enabled(true);
+#elif USE_AUDIO_SPDIF
+    output_format = audio_spdif_setup(&audio_format, &audio_spdif_default_config);
+    if (!output_format) {
+        panic("PicoAudio: Unable to open audio device.\n");
+    }
+    //ok = audio_spdif_connect(producer_pool);
+    ok = audio_spdif_connect(producer_pool);
+    assert(ok);
+    audio_spdif_set_enabled(true);
+#endif
+    return producer_pool;
+}
+
+static inline uint32_t _millis(void)
+{
+	return to_ms_since_boot(get_absolute_time());
+}
+
+int main() {
+#if PICO_ON_DEVICE
+#if USE_AUDIO_PWM
+    set_sys_clock_48mhz();
+#endif
+#endif
+
+    stdio_init_all();
+
+    // Set PLL_USB 96MHz
+    pll_init(pll_usb, 1, 1536 * MHZ, 4, 4);
+    clock_configure(clk_usb,
+        0,
+        CLOCKS_CLK_USB_CTRL_AUXSRC_VALUE_CLKSRC_PLL_USB,
+        96 * MHZ,
+        48 * MHZ);
+    // Change clk_sys to be 96MHz.
+    clock_configure(clk_sys,
+        CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX,
+        CLOCKS_CLK_SYS_CTRL_AUXSRC_VALUE_CLKSRC_PLL_USB,
+        96 * MHZ,
+        96 * MHZ);
+    // CLK peri is clocked from clk_sys so need to change clk_peri's freq
+    clock_configure(clk_peri,
+        0,
+        CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS,
+        96 * MHZ,
+        96 * MHZ);
+    // Reinit uart now that clk_peri has changed
+    stdio_init_all();
+
+    // DCDC PSM control
+    // 0: PFM mode (best efficiency)
+    // 1: PWM mode (improved ripple)
+    gpio_init(PIN_DCDC_PSM_CTRL);
+    gpio_set_dir(PIN_DCDC_PSM_CTRL, GPIO_OUT);
+    gpio_put(PIN_DCDC_PSM_CTRL, 1); // PWM mode for less Audio noise
+
+    for (int i = 0; i < SINE_WAVE_TABLE_LEN; i++) {
+        sine_wave_table[i] = 32767 * cosf(i * 2 * (float) (M_PI / SINE_WAVE_TABLE_LEN));
+    }
+
+    ap = init_audio();
+
+    while (true) {
+#if USE_AUDIO_PWM
+        enum audio_correction_mode m = audio_pwm_get_correction_mode();
+#endif
+        int c = getchar_timeout_us(0);
+        if (c >= 0) {
+            if (c == '-' && vol) vol--;
+            if ((c == '=' || c == '+') && vol < 256) vol++;
+            if (c == '[' && step0 > 0x10000) step0 -= 0x10000;
+            if (c == ']' && step0 < (SINE_WAVE_TABLE_LEN / 16) * 0x20000) step0 += 0x10000;
+            if (c == '{' && step1 > 0x10000) step1 -= 0x10000;
+            if (c == '}' && step1 < (SINE_WAVE_TABLE_LEN / 16) * 0x20000) step1 += 0x10000;
+            if (c == 'q') break;
+#if USE_AUDIO_PWM
+            if (c == 'c') {
+                bool done = false;
+                while (!done) {
+                    if (m == none) m = fixed_dither;
+                    else if (m == fixed_dither) m = dither;
+                    else if (m == dither) m = noise_shaped_dither;
+                    else if (m == noise_shaped_dither) m = none;
+                    done = audio_pwm_set_correction_mode(m);
+                }
+            }
+            printf("vol = %d, step0 = %d, step1 = %d mode = %d      \r", vol, step0 >> 16, step1 >> 16, m);
+#else
+            printf("vol = %d, step0 = %d, step1 = %d      \r\n", vol, step0 >> 16, step1 >> 16);
+#endif
+        }
+    }
+    printf("\n");
+    return 0;
+}
+
+void i2s_callback_func()
+{
+    audio_buffer_t *buffer = take_audio_buffer(ap, false);
+    if (buffer == NULL) { return; }
+    int32_t *samples = (int32_t *) buffer->buffer->bytes;
+    for (uint i = 0; i < buffer->max_sample_count; i++) {
+        int32_t value0 = (vol * sine_wave_table[pos0 >> 16u]) << 8u;
+        int32_t value1 = (vol * sine_wave_table[pos1 >> 16u]) << 8u;
+        // use 32bit full scale
+        samples[i*2+0] = value0 + (value0 >> 16u);  // L
+        samples[i*2+1] = value1 + (value1 >> 16u);  // R
+        pos0 += step0;
+        pos1 += step1;
+        if (pos0 >= pos_max) pos0 -= pos_max;
+        if (pos1 >= pos_max) pos1 -= pos_max;
+    }
+    buffer->sample_count = buffer->max_sample_count;
+    give_audio_buffer(ap, buffer);
+    return;
+}
